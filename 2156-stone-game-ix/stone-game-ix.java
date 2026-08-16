25b2fc39e359784d1e4637c8e3ae5f17c36d54b1
Kotari Veera Venkata Satyanarayana <kotariveeravenkatasatyanarayan@gmail.com>
2026-08-16 22:14:58 +0530

    Time: 8 ms (12.84%) | Memory: 114.9 MB (52.03%) - LeetSync

---
+++ 2156-stone-game-ix/stone-game-ix.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+
+        int count0 = 0;
+        int count1 = 0;
+        int count2 = 0;
+
+        // Count stones based on remainder
+        for (int stone : stones) {
+
+            if (stone % 3 == 0) {
+                count0++;
+            } else if (stone % 3 == 1) {
+                count1++;
+            } else {
+                count2++;
+            }
+        }
+
+        // Game logic
+        if (count0 % 2 == 0) {
+            return count1 > 0 && count2 > 0;
+        } else {
+            return Math.abs(count1 - count2) > 2;
+        }
+    }
+}
