cb17804d1d6510d4e520d16a06f3bdc57a0612d0
Kotari Veera Venkata Satyanarayana <kotariveeravenkatasatyanarayan@gmail.com>
2026-08-17 14:26:46 +0530

    Time: 1 ms (99.96%) | Memory: 94.7 MB (15.77%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,6 +1,6 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int buy = prices[0];
+       /* int buy = prices[0];
         int profit = 0;
         for (int i = 1; i < prices.length; i++){
             if (prices[i] < buy){
@@ -10,6 +10,14 @@ class Solution {
                 profit = prices[i] - buy;
             }
         }
-        return profit;
+        return profit; */
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for(int price : prices){
+            minPrice = Math.min(minPrice, price);
+            int profit = price - minPrice;
+            maxProfit = Math.max(maxProfit, profit);
+        }
+        return maxProfit;
     }
 }
