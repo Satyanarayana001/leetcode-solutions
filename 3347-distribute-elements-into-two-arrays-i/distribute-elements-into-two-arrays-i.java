f035b107c0aeeb1b4850c22af514ff2d873ab051
Kotari Veera Venkata Satyanarayana <kotariveeravenkatasatyanarayan@gmail.com>
2026-08-20 21:13:42 +0530

    Time: 1 ms (98.08%) | Memory: 46.5 MB (81.25%) - LeetSync

--- 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.java
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.java
@@ -1,22 +1,39 @@
 class Solution {
     public int[] resultArray(int[] nums) {
         int n = nums.length;
-        List<Integer> arr1 = new ArrayList<>();
-        List<Integer> arr2 = new ArrayList<>();
-        arr1.add(nums[0]);
-        arr2.add(nums[1]);
-        for(int i = 2; i<n; i++){
-            int lastArr1 = arr1.get(arr1.size()-1);
-            int lastArr2 = arr2.get(arr2.size()-1);
-            if(lastArr1>lastArr2){
-                arr1.add(nums[i]);
-            }
-            else{
-                arr2.add(nums[i]);
+        
+        // Pre-allocate standard primitive arrays to avoid boxing overhead
+        int[] arr1 = new int[n];
+        int[] arr2 = new int[n];
+        
+        // Track the current size (and next insertion index) of each array
+        int size1 = 0;
+        int size2 = 0;
+        
+        // Seed the initial elements
+        arr1[size1++] = nums[0];
+        arr2[size2++] = nums[1];
+        
+        // Distribute elements using basic index pointers
+        for (int i = 2; i < n; i++) {
+            // Read the last inserted elements directly by index
+            if (arr1[size1 - 1] > arr2[size2 - 1]) {
+                arr1[size1++] = nums[i];
+            } else {
+                arr2[size2++] = nums[i];
             }
         }
-        return Stream.concat(arr1.stream(), arr2.stream())
-                     .mapToInt(Integer::intValue)
-                     .toArray();
+        
+        // Re-use the input array 'nums' as the result to achieve O(1) extra space!
+        // Copy arr1 elements back into nums
+        for (int i = 0; i < size1; i++) {
+            nums[i] = arr1[i];
+        }
+        // Copy arr2 elements right after arr1
+        for (int i = 0; i < size2; i++) {
+            nums[size1 + i] = arr2[i];
+        }
+        
+        return nums;
     }
 }
